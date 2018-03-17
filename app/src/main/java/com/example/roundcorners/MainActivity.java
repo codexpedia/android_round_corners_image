e6136d189ef5442d58fa8e0e6935fc98e6df5415
codexpedia <codexpedia.com@gmail.com>
2018-03-16 20:34:55 -0500

    images

--- app/src/main/java/com/example/roundcorners/MainActivity.java
+++ app/src/main/java/com/example/roundcorners/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView picasso = (ImageView) findViewById(R.id.iv_picasso);
         Picasso.with(this)
-                .load("http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg")
+                .load("https://user-images.githubusercontent.com/35174627/37550299-d5fd7992-2958-11e8-8024-d4a8e9f0d3eb.jpg")
                 .transform(new RoundCornersTransformation(20, 20, true, true))
                 .into(picasso);
     }
